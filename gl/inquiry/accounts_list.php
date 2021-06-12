fe3a414ba054ed2a10b65994be301eae3ea30509
notrinos <trananhphuong83@gmail.com>
2021-06-12 14:11:18 +0700

    Remove dummy variable, continue.

--- gl/inquiry/accounts_list.php
+++ gl/inquiry/accounts_list.php
@@ -24,9 +24,8 @@ page(_($help_context = 'GL Accounts'), true, false, '', $js);
 if(get_post('search'))
 	$Ajax->activate('account_tbl');
 
-// Filter form. Use query string so the client_id will not disappear
-// after ajax form post.
-start_form(false, false, $_SERVER['PHP_SELF'] . '?' . $_SERVER['QUERY_STRING']);
+// Filter form. Use query string so the client_id will not disappear after ajax form post.
+start_form(false, $_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING']);
 
 start_table(TABLESTYLE_NOBORDER);
 
@@ -50,14 +49,11 @@ $th = array('', _('Account Code'), _('Description'), _('Category'));
 table_header($th);
 
 $k = 0;
-$name = $_GET['client_id'];
-$skip = $_GET['skip'];
+$result = get_chart_accounts_search(get_post('description'), $_GET['skip']);
 
-$result = get_chart_accounts_search(get_post('description'), $skip);
 while ($myrow = db_fetch_assoc($result)) {
 	alt_table_row_color($k);
-	$value = $myrow['account_code'];
-	ahref_cell(_('Select'), 'javascript:void(0)', '', 'selectComboItem(window.opener.document, "'.$name.'", "'.$value.'")');
+	ahref_cell(_('Select'), 'javascript:void(0)', '', 'selectComboItem(window.opener.document, "'.$_GET['client_id'].'", "'.$myrow['account_code'].'")');
 	label_cell($myrow['account_code']);
 	label_cell($myrow['account_name']);
 	label_cell($myrow['name']);
